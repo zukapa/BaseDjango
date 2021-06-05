7f1298f4ea16773bff04922b1dbf3dcc94e94ab0
Zukapa <zukapa@yandex.ru>
2021-06-05 19:10:42 +0300

    added ProductCategory and Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    desc = models.TextField(blank=True, null=True)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='products_images', blank=True)
+    desc = models.CharField(max_length=64, blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
