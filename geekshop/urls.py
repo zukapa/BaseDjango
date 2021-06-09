6a949176ec34320ee4bdb1b32c4bcbd8f453c3cb
Zukapa <zukapa@yandex.ru>
2021-06-09 23:30:53 +0300

    added base.html for register.html and login.html in templates

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import index, products
+
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products'))
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
