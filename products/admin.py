0a275f4145c94e8d8dd3f2ac1cd8a2d0332785c4
Zukapa <zukapa@yandex.ru>
2021-06-21 22:10:38 +0300

    Convert Django admin

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from products.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'desc', ('price', 'quantity'), 'category')
+    readonly_fields = ('desc',)
+    ordering = ('name',)
+    search_fields = ('name',)
 
 
 
