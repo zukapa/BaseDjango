c39051988dff85760b3f8236d6686185c69efd44
Zukapa <zukapa@yandex.ru>
2021-06-23 13:52:30 +0300

    added Paginator to products

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:category_id>/', products, name='product')
+    path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page')
 ]
