fb7bd2544554b3ec85fdb2e5167b6bf4a905fb9f
Zukapa <zukapa@yandex.ru>
2021-06-23 17:58:57 +0300

    overwrite method dispatch() for ListView, CreateView, UpdateView, DeleteView

--- admins/views.py
+++ admins/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
 
 from users.models import User
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
@@ -13,11 +14,6 @@ def index(request):
     return render(request, 'admins/admin.html')
 
 
-# @user_passes_test(lambda u: u.is_superuser)
-# def admin_users(request):
-#     context = {'title': 'GeekShop - Админ | Пользователь', 'users': User.objects.all()}
-#     return render(request, 'admins/admin-users-read.html', context)
-
 class UserListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
@@ -27,18 +23,9 @@ class UserListView(ListView):
         context['title'] = 'GeekShop - Админ | Пользователи'
         return context
 
-
-# @user_passes_test(lambda u: u.is_superuser)
-# def admin_users_create(request):
-#     if request.method == 'POST':
-#         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:admin_users'))
-#     else:
-#         form = UserAdminRegisterForm()
-#     context = {'title': 'GeekShop - Админ | Регистрация', 'form': form}
-#     return render(request, 'admins/admin-users-create.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
 class UserCreateView(CreateView):
@@ -52,22 +39,9 @@ class UserCreateView(CreateView):
         context['title'] = 'GeekShop - Админ | Регистрация'
         return context
 
-
-# @user_passes_test(lambda u: u.is_superuser)
-# def admin_users_update(request, id):
-#     selected_user = User.objects.get(id=id)
-#     if request.method == 'POST':
-#         form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:admin_users'))
-#     else:
-#         form = UserAdminProfileForm(instance=selected_user)
-#     context = {'title': 'GeekShop - Админ | Обновление пользователя',
-#                'form': form,
-#                'selected_user': selected_user,
-#                }
-#     return render(request, 'admins/admin-users-update-delete.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserUpdateView(UpdateView):
@@ -81,13 +55,10 @@ class UserUpdateView(UpdateView):
         context['title'] = 'GeekShop - Админ | Обновление пользователя'
         return context
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
-# @user_passes_test(lambda u: u.is_superuser)
-# def admin_users_delete(request, id):
-#     user = User.objects.get(id=id)
-#     user.is_active = False
-#     user.save()
-#     return HttpResponseRedirect(reverse('admins:admin_users'))
 
 class UserDeleteView(DeleteView):
     model = User
@@ -99,3 +70,7 @@ class UserDeleteView(DeleteView):
         self.object.is_active = False
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
